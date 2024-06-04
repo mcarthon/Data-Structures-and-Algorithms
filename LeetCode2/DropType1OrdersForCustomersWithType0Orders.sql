d99e92a97b0a80c0ed08315b41b52c8b96583b14
Mark Carthon <mark.carthon@fnz.com>
2024-06-04 10:21:38 +1000

    need more practise with temp tables, window functions, and partitioning

---
+++ LeetCode2/DropType1OrdersForCustomersWithType0Orders.sql
@@ -0,0 +1,10 @@
+WITH temp AS (
+SELECT *,
+       MIN(order_type) OVER(PARTITION BY customer_id) AS min
+FROM Orders
+)
+SELECT order_id,
+       customer_id,
+       order_type
+FROM   temp
+WHERE  order_type = min;  
